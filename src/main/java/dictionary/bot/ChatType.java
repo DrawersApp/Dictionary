1afb46eee8e7ebb0509c5f2767fecfe8218f7703
Nishant-Pathak <nishantpathak.cse@gmail.com>
2016-04-09 23:34:15 +0530

    removing dead code

--- src/main/java/dictionary/bot/ChatType.java
+++
@@ -1,8 +0,0 @@
-package dictionary.bot;
-
-/**
- * Created by harshit on 20/1/16.
- */
-public enum ChatType {
-    TEXT
-}
--- src/main/java/dictionary/bot/TextPublishApi.java
+++
@@ -1,7 +0,0 @@
-package dictionary.bot;
-
-/**
- * Created by harshit on 5/2/16.
- */
-public class TextPublishApi {
-}
